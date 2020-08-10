178c1173b5ff64c37f01be71aa188b104421d837
Andrew <andrewyen64@gmail.com>
2020-08-10 15:25:31 -0700

    Change seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
 INSERT INTO burgers (burger_name) VALUES ('Galaxy Burger');
 INSERT INTO burgers (burger_name) VALUES ('Big Bang Burger');
-INSERT INTO burgers (burger_name) VALUES ('The Supernova');
-INSERT INTO burgers (burger_name) VALUES ('Beyond Burger');
