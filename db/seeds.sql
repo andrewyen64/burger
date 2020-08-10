1a35915ea196ab58d4b6bea96a6330728bfce15b
Andrew <andrewyen64@gmail.com>
2020-08-10 12:43:23 -0700

    Add dummy seed data

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
+INSERT INTO burgers (burger_name) VALUES ('Galaxy Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Bang Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Supernova');
+INSERT INTO burgers (burger_name) VALUES ('Beyond Burger');
