9237ef94293c47892faf25cefc10d0ed98a13d1a
Andrew <andrewyen64@gmail.com>
2020-08-10 12:42:23 -0700

    Change devoured default value to false

--- db/schema.sql
+++ db/schema.sql
@@ -10,6 +10,6 @@ CREATE TABLE burgers
 (
 	id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(100) NOT NULL,
-    devoured BOOLEAN NOT NULL,
+    devoured BOOLEAN DEFAULT FALSE,
 	PRIMARY KEY (id)
 );
