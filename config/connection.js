0c6010c79a58826e261683ef73d3e2c951676bdc
Andrew <andrewyen64@gmail.com>
2020-08-10 21:47:14 -0700

    Change format to sync with JAWS

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@ var mysql = require("mysql");
 
 
 // MySQL DB Connection Info + JAWSDB_URL
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
         host : "localhost",
         port : 3306,
         user : "root",
         password : "Ay57435743",
         database : "burger_db",
-});
+    });
+}
 
 connection.connect(function (err) {
     if (err) {
