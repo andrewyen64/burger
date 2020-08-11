a4ed9568eca9dface6b94fbefa09d7e730dec8b0
Andrew <andrewyen64@gmail.com>
2020-08-10 21:43:06 -0700

    Change connection format to deploy to heroku first

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,13 @@ var mysql = require("mysql");
 
 
 // MySQL DB Connection Info + JAWSDB_URL
-var connection;
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
+var connection = mysql.createConnection({
         host : "localhost",
         port : 3306,
         user : "root",
         password : "Ay57435743",
         database : "burger_db",
-    });
-}
+});
 
 connection.connect(function (err) {
     if (err) {
